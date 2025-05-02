1bfacd4e2ca0f9729e7fd17b5b1c584397d32f06
David Silva <d.silvas@outlook.com>
2025-05-02 16:02:06 +0100

    Solve problem 20

--- src/problems/p0020.rs
+++ src/problems/p0020.rs
@@ -1,5 +1,7 @@
 use super::Problem;
-use crate::numbers::digits_sum::digits_sum;
+use crate::numbers::digits_sum::digits_sum_biguint;
+use crate::numbers::factorial::factorial_biguint;
+use crate::numbers::type_conversions::biguint_to_i128;
 
 pub struct Solution;
 
@@ -8,6 +10,8 @@ https://projecteuler.net/problem=20
 */
 impl Problem for Solution {
     fn run(&self) -> i128 {
-        digits_sum(999999999999i128)
+        let fact_100 = factorial_biguint(100u128);
+        let fact_100_digits_sum = digits_sum_biguint(&fact_100);
+        biguint_to_i128(&fact_100_digits_sum)
     }
 }
